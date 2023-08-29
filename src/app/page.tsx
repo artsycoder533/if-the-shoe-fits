0356cebe34c0d3cf3d62191a6598616ccc4b218e
Natasha Johnson <njohnson533@gmail.com>
2023-08-29 14:52:29 -0400

    chore: fix type error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,28 @@ import PRODUCTS from "../data";
 import Navigation from "@/components/Navigation";
 import { shopifyClient, parseShopifyResponse } from "../../lib/shopify";
 
-// type Product = {
-//   id: string;
-//   availableForSale: boolean;
-//   description: string;
-//   handle: string;
-//   title: string;
-//   variants: any[];
-//   images: any[];
-// }
+type Product = {
+  id: string;
+  availableForSale: boolean;
+  description: string;
+  handle: string;
+  title: string;
+  variants: any[];
+  images: any[];
+};
 export const revalidate = 15;
 
 export default async function Home() {
   //create checkout
   const response = await shopifyClient.checkout.create();
   const checkout = parseShopifyResponse(response);
+  const checkoutID = response.id;
   console.log("checkout ==>", checkout);
   //get all productcs
   const res = await shopifyClient.product.fetchAll();
   const products = parseShopifyResponse(res);
   // console.log("shopify data ==>", products, products.length);
-  const titles = products.map((product) => product.title);
+  const titles = products.map((product: Product) => product.title);
   // console.log(titles);
 
   return (
