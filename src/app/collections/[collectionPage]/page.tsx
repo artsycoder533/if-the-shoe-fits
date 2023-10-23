9dcfa9f6ebb3748b52e3b3d1596c5ed7ed46c67e
Natasha Johnson <njohnson533@gmail.com>
2023-10-23 19:00:04 -0400

    chore: wip fix collection error

--- src/app/collections/[collectionPage]/page.tsx
+++ src/app/collections/[collectionPage]/page.tsx
@@ -3,18 +3,17 @@ import PRODUCTS from "@/data";
 import Navigation from "@/components/Navigation";
 import ProductsList from "@/components/ProductsList";
 import BreadCrumbs from "@/components/BreadCrumbs";
-import { shopifyClient, parseShopifyResponse } from "../../../../lib/shopify";
 
 const CollectionPage = async ({
   params,
 }: {
   params: { collectionPage: string };
 }) => {
-  const res = await shopifyClient.collection.fetchAllWithProducts();
-  // console.log("res===>", res);
-  const collections = parseShopifyResponse(res);
-  const collectionsFull = collections[0].products;
-  console.log("collections===>", collectionsFull);
+  // const res = await shopifyClient.collection.fetchAllWithProducts();
+  // // console.log("res===>", res);
+  // const collections = parseShopifyResponse(res);
+  // const collectionsFull = collections[0].products;
+  // console.log("collections===>", collectionsFull);
   // const collection = collections.find(collection => collection.handle === params.collectionPage);
   // const collection = collections.find(
   //   (collection: any) => collection.handle === params.collectionPage
